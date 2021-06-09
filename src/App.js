c575933519260e60e3f70ad4bb84aadd221342cf
Jacob Jordan <jacob@datausa.com>
2021-06-09 11:52:51 -0600

    SWR ReactJs initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import { useRequest } from './useRequest'
+// import './styles.css'
+// import { Pokemon } from './components/Pokemon'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//     const { data: result, error } = useRequest('/pokemon')
+
+//     if (error) return <h1>Something went wrong!</h1>
+//     if (!result) return <h1>Loading...</h1>
+
+//     return (
+//         <main className='App'>
+//             <h1>Pokedex</h1>
+//             <div>
+//                 {result.results.map((pokemon) => (
+//                     <Pokemon key={pokemon.name} pokemon={pokemon} />
+//                 ))}
+//             </div>
+//         </main>
+//     )
+// }
+// export default App
+
+/* pagination functionality */
+import React from 'react'
+import { usePagination } from './usePagination'
+import './styles.css'
+
+export default function App() {
+    const { pages, isLoadingMore, loadMore, isReachingEnd } = usePagination(
+        '/pokemon'
+    )
+
+    return (
+        <main className='App'>
+            <h1>Pokedex</h1>
+            <div>{pages}</div>
+            <button
+                onClick={loadMore}
+                disabled={isLoadingMore || isReachingEnd}
+            >
+                Load more...
+            </button>
+        </main>
+    )
 }
-
-export default App;
